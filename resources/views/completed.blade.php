609068ee003a06867721c5cb485d5a18a205054a
Jemery90 <joshua.emery@cazoo.co.uk>
2022-10-28 14:46:57 +0100

    Added a completed page and linked a css file to it

---
+++ resources/views/completed.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Completed Tasks</title>
+    <link rel="stylesheet" href="{{ asset('css/completed.css') }}">
+    </head>
+    <body>
+        <h1>Completed Tasks</h1>
+    </body>
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', [Todo::class, 'index']);
 Route::post('/todos', [Todo::class, 'store']);
 Route::put('/todos/{id}', [Todo::class, 'update']);
 Route::delete('/todos/{id}', [Todo::class, 'destroy']);
+Route::get('/completed', function () {
+    return view('completed');
+});
